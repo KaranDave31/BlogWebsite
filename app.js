78e54514a4f1a49b4eb4a6ed1de7a7607b68cab6
KaranDave31 <karanbdave007@gmail.com>
2024-02-22 17:27:45 +0530

    Basic node js server along with external files

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 // console.log("Hello worlds");
 
+require('dotenv').config();
+
 const exp = require("constants");
+const path = require("path");
 const express = require("express");
+const expressLayout = require('express-ejs-layouts');
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
+
+//templating engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine' ,'ejs');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', require('./server/routes/main'));
+
+app.get('', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); 
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
