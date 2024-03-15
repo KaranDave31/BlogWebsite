5009b94a699aa5f834e9edc5fa2d6415d90f6f34
KaranDave31 <karanbdave007@gmail.com>
2024-03-15 17:50:05 +0530

    Connected database and stored all form values into database

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
-router.get('',(req,res) => {
-    res.render('index');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root@kd',
+    database: 'blogDatabase'
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL: ' + err.stack);
+      return;
+    }
+    console.log('Connected to MySQL as id ' + connection.threadId);
+  });
+
+  router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', (req, res) => {
+    const locals = {
+        title: "Panner Posting"
+    };
+    res.render('index', { layout: false, locals });
 });
 
 
 router.get('/signup',(req,res) => {
-    res.render('signup');
+    res.render('signup', { layout: false});
 });
+
+router.post('/signup', (req, res) => {
+    const { username, email, password, full_name } = req.body;
+  
+    // Insert user data into MySQL
+    const query = 'INSERT INTO Users (username, email, password, full_name) VALUES (?, ?, ?, ?)';
+    connection.query(query, [username, email, password, full_name], (error, results, fields) => {
+      if (error) {
+        console.error('Error inserting user data: ' + error.stack);
+        res.status(500).send('Error saving user data');
+        return;
+      }
+      console.log('User signed up successfully');
+      res.redirect('/signup-success'); // Redirect to a success page or any other page you prefer
+    });
+  });
+
 
 router.get('/blog',(req,res) => {
     res.render('blog');
