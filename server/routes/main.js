c6129fe254779dc3e198b1a62f8b3af7e3eb7a5b
KaranDave31 <karanbdave007@gmail.com>
2024-03-31 08:40:59 +0530

    added favicon

--- server/routes/main.js
+++ server/routes/main.js
@@ -10,7 +10,7 @@ const nodemailer = require('nodemailer');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password@123',
+    password: 'root@kd',
     database: 'blogDatabase'
   });
 
